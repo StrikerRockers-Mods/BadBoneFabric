aeb4255c4c51a82172da8546a3613263acd2d0f9
StrikerRocker <strikerrockerplays@gmail.com>
2021-11-01 12:02:40 +0530

    Fix glasses not being right click to equip. Stop effects applying to non-survival players.

--- src/main/java/subaraki/badbone/events/PlayerEvents.java
+++ src/main/java/subaraki/badbone/events/PlayerEvents.java
@@ -30,56 +30,59 @@ public class PlayerEvents {
                     ClientEvent.clientPlayerUpdate(player);
                 }
                 if (player instanceof ServerPlayer serverPlayer) {
-                    if (!serverPlayer.hasEffect(BadBone.BACK_HURT)) {
-                        if (serverPlayer.getRandom().nextInt(ModConfig.frequencyHurt) == 0 || serverPlayer.getStats().getValue(Stats.CUSTOM.get(Stats.TIME_SINCE_REST)) < 100) {
-                            float fullInv = 4 * 9 * 64; //four rows of 9 slots with 64 items in
-                            float totalCarrying = 0;
-                            for (ItemStack stack : serverPlayer.getInventory().items) {
-                                totalCarrying += stack.isEmpty() ? 0f : 64f * ((float) stack.getCount() / (float) stack.getMaxStackSize());
-                            }
-                            //the closer to 0 weight is, the more the player carries
-                            //if the weight is taking up 2/3-ish from the inventory, then you can start having back pain
-                            if (totalCarrying > fullInv * 0.65f) {
-                                if (serverPlayer.getRandom().nextInt(ModConfig.chanceHurt) == 0) {
-                                    serverPlayer.addEffect(new MobEffectInstance(BadBone.BACK_HURT, serverPlayer.getRandom().nextInt(20 * 60 * 2) + 20 * 15, 0, false, false, true));
-                                    playHurtSound(serverPlayer);
+                    if (isInSurvivalMode(player)) {
+                        if (!serverPlayer.hasEffect(BadBone.BACK_HURT)) {
+                            if (serverPlayer.getRandom().nextInt(ModConfig.frequencyHurt) == 0 || serverPlayer.getStats().getValue(Stats.CUSTOM.get(Stats.TIME_SINCE_REST)) < 100) {
+                                float fullInv = 4 * 9 * 64; //four rows of 9 slots with 64 items in
+                                float totalCarrying = 0;
+                                for (ItemStack stack : serverPlayer.getInventory().items) {
+                                    totalCarrying += stack.isEmpty() ? 0f : 64f * ((float) stack.getCount() / (float) stack.getMaxStackSize());
+                                }
+                                //the closer to 0 weight is, the more the player carries
+                                //if the weight is taking up 2/3-ish from the inventory, then you can start having back pain
+                                if (totalCarrying > fullInv * 0.65f) {
+                                    if (serverPlayer.getRandom().nextInt(ModConfig.chanceHurt) == 0) {
+                                        serverPlayer.addEffect(new MobEffectInstance(BadBone.BACK_HURT, serverPlayer.getRandom().nextInt(20 * 60 * 2) + 20 * 15, 0, false, false, true));
+                                        playHurtSound(serverPlayer);
+                                    }
                                 }
                             }
                         }
-                    }
 
-                    if (serverPlayer.hasEffect(BadBone.ARTHRITIS)) {
-                        if (serverPlayer.getMainHandItem().getAttributeModifiers(EquipmentSlot.MAINHAND).containsKey(Attributes.ATTACK_DAMAGE)) {
-                            serverPlayer.drop(serverPlayer.getMainHandItem(), true);
-                            serverPlayer.setItemInHand(InteractionHand.MAIN_HAND, ItemStack.EMPTY);
-                            playHurtSound(serverPlayer);
+
+                        if (serverPlayer.hasEffect(BadBone.ARTHRITIS)) {
+                            if (serverPlayer.getMainHandItem().getAttributeModifiers(EquipmentSlot.MAINHAND).containsKey(Attributes.ATTACK_DAMAGE)) {
+                                serverPlayer.drop(serverPlayer.getMainHandItem(), true);
+                                serverPlayer.setItemInHand(InteractionHand.MAIN_HAND, ItemStack.EMPTY);
+                                playHurtSound(serverPlayer);
+                            }
+                            if (serverPlayer.getOffhandItem().getAttributeModifiers(EquipmentSlot.OFFHAND).containsKey(Attributes.ATTACK_DAMAGE)) {
+                                serverPlayer.drop(serverPlayer.getOffhandItem(), true);
+                                serverPlayer.setItemInHand(InteractionHand.OFF_HAND, ItemStack.EMPTY);
+                                playHurtSound(serverPlayer);
+                            }
+                        } else {
+                            if (serverPlayer.isUsingItem() && serverPlayer.getRandom().nextInt(ModConfig.frequencyArthritis) == 0) {
+                                serverPlayer.addEffect(new MobEffectInstance(BadBone.ARTHRITIS, serverPlayer.getRandom().nextInt(20 * 60 * 2) + 20 * 15, 0, false, false, true));
+                            }
                         }
-                        if (serverPlayer.getOffhandItem().getAttributeModifiers(EquipmentSlot.OFFHAND).containsKey(Attributes.ATTACK_DAMAGE)) {
-                            serverPlayer.drop(serverPlayer.getOffhandItem(), true);
-                            serverPlayer.setItemInHand(InteractionHand.OFF_HAND, ItemStack.EMPTY);
-                            playHurtSound(serverPlayer);
+
+                        if (!serverPlayer.hasEffect(BadBone.BLIND)) {
+                            if (serverPlayer.getRandom().nextInt(ModConfig.frequencyEyes) == 0) {
+                                serverPlayer.addEffect(new MobEffectInstance(BadBone.BLIND, serverPlayer.getRandom().nextInt(20 * 60 * 15) + 20 * 60 * 15, 0, false, false, true));
+                                serverPlayer.getAdvancements().award(serverPlayer.getServer().getAdvancements().getAdvancement(ADVANCEMENT), "eyesight");
+                            }
                         }
-                    } else {
-                        if (serverPlayer.isUsingItem() && serverPlayer.getRandom().nextInt(ModConfig.frequencyArthritis) == 0) {
-                            serverPlayer.addEffect(new MobEffectInstance(BadBone.ARTHRITIS, serverPlayer.getRandom().nextInt(20 * 60 * 2) + 20 * 15, 0, false, false, true));
+                        if (serverPlayer.hasEffect(BadBone.CHRONO)) {
+                            if (!serverPlayer.getAttribute(Attributes.MOVEMENT_SPEED).hasModifier(MobEffects.MOVEMENT_SLOWDOWN.getAttributeModifiers().get(Attributes.MOVEMENT_SPEED)))
+                                serverPlayer.getAttribute(Attributes.MOVEMENT_SPEED).addTransientModifier(MobEffects.MOVEMENT_SLOWDOWN.getAttributeModifiers().get(Attributes.MOVEMENT_SPEED));
+                        } else {
+                            if (serverPlayer.getAttribute(Attributes.MOVEMENT_SPEED).hasModifier(MobEffects.MOVEMENT_SLOWDOWN.getAttributeModifiers().get(Attributes.MOVEMENT_SPEED)))
+                                serverPlayer.getAttribute(Attributes.MOVEMENT_SPEED).removeModifier(MobEffects.MOVEMENT_SLOWDOWN.getAttributeModifiers().get(Attributes.MOVEMENT_SPEED));
+
                         }
                     }
-
-                    if (!serverPlayer.hasEffect(BadBone.BLIND)) {
-                        if (serverPlayer.getRandom().nextInt(ModConfig.frequencyEyes) == 0) {
-                            serverPlayer.addEffect(new MobEffectInstance(BadBone.BLIND, serverPlayer.getRandom().nextInt(20 * 60 * 15) + 20 * 60 * 15, 0, false, false, true));
-                            serverPlayer.getAdvancements().award(serverPlayer.getServer().getAdvancements().getAdvancement(ADVANCEMENT), "eyesight");
-                        }
-                    }
-                    if (serverPlayer.hasEffect(BadBone.CHRONO)) {
-                        if (!serverPlayer.getAttribute(Attributes.MOVEMENT_SPEED).hasModifier(MobEffects.MOVEMENT_SLOWDOWN.getAttributeModifiers().get(Attributes.MOVEMENT_SPEED)))
-                            serverPlayer.getAttribute(Attributes.MOVEMENT_SPEED).addTransientModifier(MobEffects.MOVEMENT_SLOWDOWN.getAttributeModifiers().get(Attributes.MOVEMENT_SPEED));
-                    } else {
-                        if (serverPlayer.getAttribute(Attributes.MOVEMENT_SPEED).hasModifier(MobEffects.MOVEMENT_SLOWDOWN.getAttributeModifiers().get(Attributes.MOVEMENT_SPEED)))
-                            serverPlayer.getAttribute(Attributes.MOVEMENT_SPEED).removeModifier(MobEffects.MOVEMENT_SLOWDOWN.getAttributeModifiers().get(Attributes.MOVEMENT_SPEED));
-
-                    }
-
+                    //heal even if in creative
                     BlockPos bubble = serverPlayer.blockPosition();
                     BlockPos magma = serverPlayer.getOnPos();
                     Level level = serverPlayer.level;
@@ -97,17 +100,18 @@ public class PlayerEvents {
             }
         });
         ItemCraftedEvent.EVENT.register((player, crafted) -> {
-            if (crafted.getItem().equals(Items.CLOCK)) {
+            if (crafted.getItem().equals(Items.CLOCK) && isInSurvivalMode(player)) {
                 player.addEffect(new MobEffectInstance(BadBone.CHRONO, 20 * 90, 0, false, false, true));
             }
         });
         ItemPickupEvent.EVENT.register((player, pickedUpStack) -> {
-            if (pickedUpStack.getItem().equals(Items.CLOCK)) {
+            if (pickedUpStack.getItem().equals(Items.CLOCK) && isInSurvivalMode(player)) {
                 player.addEffect(new MobEffectInstance(BadBone.CHRONO, 20 * 90, 0, false, false, true));
             }
         });
         PlayerHurtEvent.EVENT.register((player, source, amt) -> {
-            if (player.fallDistance > 3 && source == DamageSource.FALL && player.getRandom().nextInt(ModConfig.frequencyKnee) == 0) {
+            //check for damage > 0.4f to prevent non damage falls like slime blocks to generate knee pain
+            if (player.fallDistance > 3 && source == DamageSource.FALL && amt > 0.4F && player.getRandom().nextInt(ModConfig.frequencyKnee) == 0) {
                 player.addEffect(new MobEffectInstance(BadBone.KNEE_HURT, player.getRandom().nextInt(20 * 15) * (int) player.fallDistance, 0, false, false, true));
                 playHurtSound(player);
             }
@@ -117,5 +121,9 @@ public class PlayerEvents {
     public static void playHurtSound(Player player) {
         player.playNotifySound(SoundEvents.BONE_BLOCK_BREAK, SoundSource.PLAYERS, 1f, player.getRandom().nextFloat() + 1f);
         player.playNotifySound(SoundEvents.PLAYER_HURT, SoundSource.PLAYERS, 1f, player.getRandom().nextFloat() + 1f);
+    }
+
+    public static boolean isInSurvivalMode(Player player) {
+        return !player.isSpectator() && !player.isCreative();
     }
 }
--- src/main/java/subaraki/badbone/items/GlassesItem.java
+++ src/main/java/subaraki/badbone/items/GlassesItem.java
@@ -1,14 +1,37 @@
 package subaraki.badbone.items;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 
 public class GlassesItem extends Item {
     private static final Properties PROPERTIES = new FabricItemSettings().equipmentSlot(stack -> EquipmentSlot.HEAD).tab(CreativeModeTab.TAB_TOOLS).stacksTo(1);
 
     public GlassesItem() {
         super(PROPERTIES);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        ItemStack glasses = player.getItemInHand(hand);
+        ItemStack helmet = player.getItemBySlot(EquipmentSlot.HEAD);
+        if (helmet.isEmpty()) {
+            player.setItemSlot(EquipmentSlot.HEAD, glasses.copy());
+            if (!level.isClientSide()) {
+                player.awardStat(Stats.ITEM_USED.get(this));
+            }
+
+            glasses.setCount(0);
+            return InteractionResultHolder.sidedSuccess(glasses, level.isClientSide());
+        } else {
+            return InteractionResultHolder.fail(glasses);
+        }
     }
 }
